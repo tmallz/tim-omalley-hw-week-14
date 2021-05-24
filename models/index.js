2f3558d7acec001b4741693f43b0bee96ee8a7b5
Tim O'Malley <tomalley24@gmail.com>
2021-05-24 11:03:22 -0500

    added table relationships between user, comment, and post

--- models/index.js
+++ models/index.js
@@ -4,10 +4,32 @@ const Comment = require('./Comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
-    onDelete: 
-})
+    onDelete: 'CASCADE'
+});
 
+User.hasMany(Comment,{
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
 
 module.exports = { User, Post, Comment };
