a4c76136c95cb796a0b3ef8eabe94825fb3b3252
Tim O'Malley <tomalley24@gmail.com>
2021-05-24 10:51:45 -0500

    added post and comment models

---
+++ models/Comment.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false, 
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'post',
+            key: 'id',
+        }
+    },
+},
+    {
+        sequelize,
+        timestamps: true,
+        modelName: 'post'
+    }
+)
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-module.exports = { User };
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 
+})
+
+
+
+
+module.exports = { User, Post, Comment };
